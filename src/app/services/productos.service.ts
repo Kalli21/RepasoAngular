edf6442c077bed21a9aa525208eee1152977ffe3
Kalli <kalli_28456@hotmail.com>
2022-03-23 23:55:10 -0500

    Fin Seccion 7 Udemy Ang01

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,21 +9,75 @@ export class ProductosService {
 	
   cargando = true;
   productos: Producto[]=[];
+  productosFiltrado: Producto[]=[];
+
 
   constructor(private http:HttpClient) {
     this.cargarProductos();
    }
 
   private cargarProductos() {
-    this.http.get<Producto[]>('https://angular-html-57b14-default-rtdb.firebaseio.com/productos_idx.json')
+
+    return new Promise<void>((resolve,reject)=>{
+
+      this.http.get<Producto[]>('https://angular-html-57b14-default-rtdb.firebaseio.com/productos_idx.json')
       .subscribe((resp:Producto[])=>{
         console.log(resp);
         
         this.productos = resp;
         this.cargando = false;
+        resolve();
+      });
+    });
 
+
+    
+  }
+
+    getProducto(id:string){
+      return this.http.get(`https://angular-html-57b14-default-rtdb.firebaseio.com/productos/${id}.json`);
+    }
+
+    buscarProducto(termino:string){
+      
+
+      if(this.productos.length == 0){
+        //cargar productos
+        this.cargarProductos().then(()=>{
+          //ejecutar despues de tener los productos
+          //aplicar filtro
+          this.filtrarProductos(termino);
+        });
+      }else{
+        //aplicar filtro
+        this.filtrarProductos(termino);
+      }
+
+      // this.productosFiltrado =this.productos.filter(producto=>{
+        
+
+      //   console.log(this.productosFiltrado);
+      //   return true;
+
+      // });
+
+    }
+
+    private filtrarProductos(termino:string){
+      
+      this.productosFiltrado = [];
+        
+      termino = termino.toLocaleLowerCase();
+
+      this.productos.forEach(prod =>{
+        
+        
+        if (prod.categoria && prod.categoria.indexOf(termino)>=0 || prod.titulo && prod.titulo.toLocaleLowerCase().indexOf(termino)>=0){
+          this.productosFiltrado.push(prod);
+        }
 
       });
-  }
+
+    }
 
 }
